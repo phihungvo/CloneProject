2a3a085c35fe7ba45bc48d6a7ca23f17069b82df
phihungvo <vophihung987@gmail.com>
2024-08-14 13:30:42 +0700

    initial commit

---
+++ src/main/java/com/luv2code/MiniProject/MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.MiniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+
+}
